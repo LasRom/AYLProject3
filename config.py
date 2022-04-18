65d4bb30d6a7d588d59a203b59127a74ded79314
LasRom <lastuvkaroman38@gmail.com>
2022-04-18 18:11:37 +0300

    config fix

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # лежат все ip, логины и пароли
-api_weather = "9cee83e0fe5a8193db10637d4f4ab31b"
-api_token = "5264678384:AAGa3C3-LGgAtR3Qaz2YuR2BKY5EZpn6Qno"
-login_proxy = "UhMGfq"
-password_proxy = "uLJ0Rv"
-email = 'rlastuvka43@gmail.com'
-password_email = 'LoLoLoshka12345'
+api_weather = ""
+api_token = ""
+login_proxy = ""
+password_proxy = ""
+email = ''
+password_email = ''
